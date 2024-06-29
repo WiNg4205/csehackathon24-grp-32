92061e8bbf7660f3f09d4284c9580a425d99fb52
James Phung <z5419647@ad.unsw.edu.au>
2024-06-29 12:04:31 +1000

    Started friends list page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login.jsx';
 import Register from './components/Register.jsx';
+import FriendsList from './components/FriendsList/FriendsList.jsx';
 
 function App () {
   return (
@@ -10,6 +11,7 @@ function App () {
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register handleSubmit={null}/>} />
+        <Route path='/friends' element={<FriendsList />} />
         <Route path="*" element={<Navigate to='/login' />} />
       </Routes>
     </BrowserRouter>
