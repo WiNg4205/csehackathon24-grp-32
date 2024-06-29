c6bab467e422f7a4eb5d0c6850a74d367a0c578d
Brian Vu~ <berriiacn@gmail.com>
2024-06-30 00:59:07 +1000

    Implemented popup to upload image

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,41 +1,56 @@
 import React from 'react';
 import './Dashboard.css'; // Assuming you have a CSS file for this component
 import NavBar from './NavBar.jsx';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 const Dashboard = () => {
-    const navigate = useNavigate(); // Initialize useNavigate
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    const handleBoxClick = (boxId) => {
-        if (boxId === 1) {
-            navigate("/habits/physical");
-        } else if (boxId === 2) {
-            navigate("/habits/mental");
-        } else if (boxId === 3) {
-            navigate("/habits/intellectual");
-        } else if (boxId === 4) {
-            navigate("/habits/social");
-        } else if (boxId === 5) {
-            navigate("/habits/financial");
-        } else if (boxId === 6) {
-            navigate("/habits/spiritual");
-        }
+    const handleBoxClick = () => {
     };
 
     return (
         <div className="dashboard-container">
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Upload image</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <Form>
+                    <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                    >
+                    <input type="file" name="edit_image" id="update-image" accept="image/png, image/jpg, image/jpeg" />
+                    </Form.Group>
+                </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Upload
+                </Button>
+                </Modal.Footer>
+            </Modal>
             <div className="header">
                 <h1>Habits</h1>
             </div>
             <div className="row">
-                <button className="box" onClick={() => handleBoxClick(1)}>Empty</button>
-                <button className="box" onClick={() => handleBoxClick(2)}>Empty</button>
-                <button className="box" onClick={() => handleBoxClick(3)}>Empty</button>
+                <Button variant="primary" className='box' onClick={handleShow}>Empty</Button>
+                <Button variant="primary" className='box' onClick={handleShow}>Empty</Button>
+                <Button variant="primary" className='box' onClick={handleShow}>Empty</Button>
             </div>
             <div className="row">
-                <button className="box" onClick={() => handleBoxClick(4)}>Empty</button>
-                <button className="box" onClick={() => handleBoxClick(5)}>Empty</button>
-                <button className="box" onClick={() => handleBoxClick(6)}>Empty</button>
+                <Button variant="primary" className='box' onClick={handleShow}>Empty</Button>
+                <Button variant="primary" className='box' onClick={handleShow}>Empty</Button>
+                <Button variant="primary" className='box' onClick={handleShow}>Empty</Button>
             </div>
             <NavBar />
         </div>
