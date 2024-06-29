25ae105b2080cf65331075213f8c30665e7000a5
Tom Nguyen The <z5421728@ad.unsw.edu.au>
2024-06-29 13:37:55 +1000

    rearranged routes and added login+register route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
 import express from 'express'
 
+import { login, register } from './auth'
+
 const PORT = 3000
 const HOST = 'localhost'
 
 
 const app = express()
+
+// test request
+app.get('/test', (req, res, next) => {
+  try {
+    return res.json('Hello Browser');
+  } catch (err) {
+    next(err);
+  }
+});
+
+// login request
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  res.json(login(email, password));
+});
+
+// register request
+app.put('/register', (req, res) => {
+  const { email, password, username, nameFirst, nameLast } = req.body;
+  res.json(register(email, password, username, nameFirst, nameLast));
+});
 
 const server = app.listen(PORT, HOST, () => {
   console.log(`Server listening on port ${PORT} at ${HOST}`);
@@ -15,12 +38,4 @@ process.on('SIGINT', () => {
   server.close(() => console.log('Shutting down server gracefully.'));
 });
 
-// Example get request
-app.get('/test', (req, res, next) => {
-  try {
-    return res.json('Hello Browser');
-  } catch (err) {
-    next(err);
-  }
-});
 
